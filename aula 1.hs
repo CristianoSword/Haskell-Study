bbee17531c15b6d6f2525d768f4b2b901c860eb1
CristianoSword <originalvgo@gmail.com>
2022-09-06 17:19:19 -0300

    up

--- aula 1.hs
+++ aula 1.hs
@@ -1,2 +1,14 @@
 polinomio :: Int -> Int
 polinomio x = x*x + 10*x + 2
+
+-- calcula o quadrado de um numero inteiro
+quadrado :: Int -> Int
+quadrado n = n*n
+
+-- soma
+soma :: Float -> Float -> Float
+coma a b =  a + b
+
+-- triplica
+tri´plica :: Int -> Int
+triplica var = var*3
