a04d7432a45fa97637e8185560780a7d7d67f232
CristianoSword <originalvgo@gmail.com>
2022-09-08 17:10:18 -0300

    fix aula1

--- aula 1.hs
+++ aula 1.hs
@@ -7,12 +7,8 @@ quadrado n = n*n
 
 -- soma
 soma :: Float -> Float -> Float
-coma a b =  a + b
+soma a b =  a + b
 
 -- triplica
 triplica :: Int -> Int
-<<<<<<< HEAD
 triplica var = var*3
-=======
-triplica var = var*3
->>>>>>> b372261228932e06bc44d6d8bc7829fbaa8aa42a
