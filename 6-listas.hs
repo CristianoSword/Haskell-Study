702a442b0a7c64cda3caf41bd49042b83a053cd5
CristianoSword <originalvgo@gmail.com>
2022-09-12 10:41:33 -0300

    fix2

--- 6-listas.hs
+++ 6-listas.hs
@@ -4,3 +4,10 @@ compr :: [Int] -> Int
 compr [] = 0
 compr (head:tail) = 1 + compr tail
 
+-- calculando o cubo e cubo de uma lista
+cubo:: Int -> Int
+cubo x = x * x * x
+
+aoCubo :: [Int] -> [Int]
+aoCubo [] = []
+aoCubo (head:tail) = cubo(head) : aoCubo tail
