ab6e693c005949172e9df8378e1b63535bdaa79e
CristianoSword <originalvgo@gmail.com>
2022-09-12 10:19:31 -0300

    fix

---
+++ 5-recursao.hs
@@ -0,0 +1,11 @@
+-- recursao comum
+potencia2 :: Int -> Int
+potencia2 n
+    | n == 0 = 1
+    | n  > 0 = 2 * potencia2 (n-1)
+    
+-- Recursao em calda
+potencia2cauda :: Int -> Int -> Int
+potencia2cauda n acumulado
+    | n == 0 = acumulado
+    | n  > 0 = potencia2cauda (n-1) (2*acumulado)
