e603bb677572071fe62b20cfd60f00231a26a271
Cristiano Camacho <originalvgo@gmail.com>
2023-02-19 20:25:37 -0300

    Update 06-listas.hs

--- 06-listas.hs
+++ 06-listas.hs
@@ -18,3 +18,10 @@ qsort [] = []
 qsort (head:tail) = qsort [y | y <- tail, y < head] --menores que pivo
                     ++ [head] -- o pivo
                     ++ qsort [y | y <- tail, y >= head]
+                    
+-- A lista maior é simplesmente cortada para combinar com o tamanho da outra menor. Como Haskell é preguiçoso, nós podemos "zippar" listas finitas com listas infinitas:
+zip [5,3,2,6,2,7,2,5,4,6,6] ["eu","sou","uma", "tartaruga"]  
+--output ghci> [(5,"eu"),(3,"sou"),(2,"uma"),(6,"tartaruga")]  
+
+zip [1..] ["banana", "laranja", "melancia", "uva"]  
+--output ghci> [(1,"banana"),(2,"laranja"),(3,"melancia"),(4,"uva")]  
