8eef5f6be8b79ebd0c2fcf556a6e8ac7b50d4794
Cristiano Camacho <originalvgo@gmail.com>
2023-02-23 17:28:40 -0300

    Update 12-foldr  foldl e Zipper.hs

--- 12-foldr  foldl e Zipper.hs
+++ 12-foldr  foldl e Zipper.hs
@@ -2,3 +2,14 @@
 myzip = foldr step (const []) :: [a] -> [b] -> [(a,b)]
     where step a f (b:bs) = (a,b):(f bs)
           step a f [] = []
+
+-- fold
+"fold" (+) [1,2,3,4,5]
+-- => 1+2+3+4+5
+
+"fold" (-) [1,2,3,4,5]
+-- => 1-2-3-4-5   ???
+
+foldr ->  1 - (2 - (3 - (4 - ( 5 - init)))
+
+foldl -> (((init - 1) - 2) - 3) - 4) - 5
